35feafb94d03e734e127fb09ebfa1051cf404ccc
mhrdd <mhrddmhrdd5610mhrddmhrdd@gmail.com>
2023-03-14 11:51:25 +0330

    updating ContactUs

--- src/components/contactus/Address.js
+++ src/components/contactus/Address.js
@@ -6,9 +6,9 @@ import styles from "./Address.module.css";
 const Address = () => {
     return (
         <div className={styles.address}>
-            <p><i className="fas fa-map-marker-alt"></i> آدرس : کرمانشاه خیابان مدرس</p>
+            <p><i className="fas fa-map-marker-alt"></i> آدرس : تهران</p>
             <p><i className='fas fa-phone'></i> شماره تماس : 09123456789</p>
-            <p><i className="fas fa-envelope"></i>ایمیل : info.mehranazizi@yahoo.com</p>
+            <p><i className="fas fa-envelope"></i>ایمیل : mhrddmhrdd0@gmail.com</p>
         </div>
     );
 };
